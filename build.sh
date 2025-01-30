38f2a512cda9d5bea5ffe4b333cd8bfde3ed088e
dreamyukii <49896401+dreamyukii@users.noreply.github.com>
2025-01-31 05:27:45 +0700

    switch to almalinux image

--- build.sh
+++ build.sh
@@ -10,7 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y chromium
+dnf group install -y kde-desktop-environment \
+    plymouth \
+    plymouth-system-theme \
+
 
 # Use a COPR Example:
 #
